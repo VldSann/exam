654e58d3f9e6b82a9863de697dc06ed140fd56e3
VldSann <71924927+VldSann@users.noreply.github.com>
2023-03-12 21:47:13 +0300

    Add files via upload

---
+++ web/app/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, flash, request, redirect, url_for, Response, make_response
+from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import current_user, login_required
+from sqlalchemy.exc import SQLAlchemyError
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+application = app
+client = app.test_client()
+app.config.from_pyfile('config.py')
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db)
+
+from models import User
+from auth import bp as auth_bp, init_login_manager, check_rights
+
+init_login_manager(app)
+app.register_blueprint(auth_bp)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route("/users")
+def users():
+    users = User.query.all()
+    return render_template("users.html", users=users)
+
+@app.route('/book', methods=['GET'])
+def tasks():
+    return render_template('book.html', items=book.query.all())
